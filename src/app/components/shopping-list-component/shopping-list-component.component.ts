82d5141c54921acb06f02ffdf65a253dcc930ff8
Gristeh <Gristeh@users.noreply.github.com>
2021-05-16 16:43:41 +0100

    ListCreation

--- src/app/components/shopping-list-component/shopping-list-component.component.ts
+++ src/app/components/shopping-list-component/shopping-list-component.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ListItem } from "src/app/models/list-item";
 
 @Component({
-  selector: 'app-shopping-list-component',
-  templateUrl: './shopping-list-component.component.html',
-  styleUrls: ['./shopping-list-component.component.css']
+  selector: "app-shopping-list-component",
+  templateUrl: "./shopping-list-component.component.html",
+  styleUrls: ["./shopping-list-component.component.css"],
 })
 export class ShoppingListComponentComponent implements OnInit {
+  // currentItemList: ListItem[] = [];
+  // previousItemList: ListItem[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // this.previousItemList=[ItemID: number;
+    //   ItemName: string;
+    //   ListID: number;
+    //   HighPriority: boolean;
+    //   Index:]
   }
 
+  ngOnInit() {}
 }
